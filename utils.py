5dd44325c3b03042f138446ee7ea9eebbb27862c
hoppe <wanghp000@gmail.com>
2018-11-30 09:22:32 +0800

    add rgb_384

--- utils.py
+++ utils.py
@@ -30,7 +30,8 @@ def get_one_example(file, crop_size = 384, output_num = 4):
     H = raw.sizes.raw_height
     W = raw.sizes.raw_width
 
-    rgbs = np.zeros([output_num, int(crop_size/4), int(crop_size/4), 3])
+    rgbs_96 = np.zeros([output_num, int(crop_size/4), int(crop_size/4), 3])
+    rgbs_384 = np.zeros([output_num, crop_size, crop_size, 3])
     raws = np.zeros([output_num, crop_size, crop_size, 1])
 
     for i in range(output_num):
@@ -38,15 +39,21 @@ def get_one_example(file, crop_size = 384, output_num = 4):
         yy = np.random.randint(0, H - crop_size)
 
         raw_matrix = rgb_full[yy:yy + crop_size, xx:xx + crop_size, :]
-        raw_matrix = tl.prepro.imresize(raw_matrix, [int(crop_size/4), int(crop_size/4)])
-        raw_matrix = raw_matrix /255
-        rgbs[i] = raw_matrix
+
+        raw_matrix_96 = tl.prepro.imresize(raw_matrix, [int(crop_size/4), int(crop_size/4)])
+        raw_matrix_96 = raw_matrix_96 /255
+
+        raw_matrix = raw_matrix/255
+
+        rgbs_96[i] = raw_matrix_96
+        rgbs_384[i] = raw_matrix
 
         raw_matrix = raw.raw_image_visible.astype(np.float32)
         raw_matrix = raw_matrix[yy:yy + crop_size, xx:xx + crop_size]
         raw_matrix = np.maximum(raw_matrix - 512, 0) / (16383 - 512)
 #         print(raw_matrix.shape)
         raw_matrix = np.expand_dims(np.float32(raw_matrix), axis=2)
+
         raws[i] = raw_matrix
 
-    return raws, rgbs
+    return raws, rgbs_384, rgbs_96
