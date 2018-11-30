d4499983d288bd3c532be2f9d2a38f3570439711
hoppe <wanghp000@gmail.com>
2018-11-30 11:48:09 +0800

    test.ipynb - define training op

---
+++ train.py
@@ -0,0 +1,11 @@
+import os, time, pickle, random, time
+from datetime import datetime
+import numpy as np
+from time import localtime, strftime
+import logging, scipy
+
+import tensorflow as tf
+import tensorlayer as tl
+from model import SRGAN_g, SRGAN_d, Vgg19_simple_api
+from utils import *
+# from config import config, log_config
