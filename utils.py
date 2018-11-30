c2759c2d2c329e939dcbce948c376a5842b04145
hoppe <hwa125@sfu.ca>
2018-11-29 22:34:34 -0800

    fix raw img shape bugs

--- utils.py
+++ utils.py
@@ -10,6 +10,39 @@ import numpy as np
 from tensorlayer import logging
 import os
 
+
+def get_inputs_labels(file_dir, raw_file_list, crop_num):
+    '''
+    Parameters
+    ----------
+    file_dir : str
+        Data dir.
+    raw_file_list : list
+        List of name of the files need to read
+    crop_num : int
+        For every img, [crop_num] number of img are randomly cropped
+
+    Return
+    ----------
+    inputs_rgbs: np.array
+        Input array of shape [len(raw_file_list)*crop_num, 96, 96, 3]
+    label_raws: np.array
+        Label array of shape [len(raw_file_list)*crop_num, 384, 384, 1]
+
+    '''
+    inputs_rgbs_lst = []
+    label_raws_lst = []
+
+    for fn in raw_file_list:
+        raws, rgbs = get_one_example(file = file_dir + os.path.sep + fn, output_num = crop_num)
+        inputs_rgbs_lst.append(rgbs)
+        label_raws_lst.append(raws)
+
+    inputs_rgbs = np.concatenate(inputs_rgbs_lst, axis=0)
+    label_raws = np.concatenate(label_raws_lst, axis=0)
+
+    return inputs_rgbs, label_raws
+
 def load_pretrain_model(sess, npz_file, network):
     '''
     Assign the given parameters to the TensorLayer network.
@@ -44,7 +77,10 @@ def get_one_example(file, crop_size = 384, output_num = 4):
     '''
     Read a raw image
     return:
-    ramdomly croped output_numx384x384x1 RAW, and output_numx96x96x3 RGB
+    1, raws:
+        ramdomly croped output_numx384x384x1 RAW
+    2, rgbs:
+        output_numx96x96x3 RGB
     '''
 
     raw = rawpy.imread(file)
@@ -59,8 +95,8 @@ def get_one_example(file, crop_size = 384, output_num = 4):
 #     print(raw.sizes)
 
     #crop the img
-    H = raw.sizes.raw_height
-    W = raw.sizes.raw_width
+    H = raw_full.shape[0]
+    W = raw_full.shape[1]
 
     rgbs = np.zeros([output_num, int(crop_size/4), int(crop_size/4), 3])
     raws = np.zeros([output_num, crop_size, crop_size, 1])
@@ -69,13 +105,12 @@ def get_one_example(file, crop_size = 384, output_num = 4):
         xx = np.random.randint(0, W - crop_size)
         yy = np.random.randint(0, H - crop_size)
 
-        raw_matrix = rgb_full[yy:yy + crop_size, xx:xx + crop_size, :]
-        raw_matrix = tl.prepro.imresize(raw_matrix, [int(crop_size/4), int(crop_size/4)])
-        raw_matrix = raw_matrix /255
-        rgbs[i] = raw_matrix
+        rgb_matrix = rgb_full[yy:yy + crop_size, xx:xx + crop_size, :]
+        rgb_matrix = tl.prepro.imresize(rgb_matrix, [int(crop_size/4), int(crop_size/4)])
+        rgb_matrix = rgb_matrix /255
+        rgbs[i] = rgb_matrix
 
-        raw_matrix = raw.raw_image_visible.astype(np.float32)
-        raw_matrix = raw_matrix[yy:yy + crop_size, xx:xx + crop_size]
+        raw_matrix = raw_full[yy:yy + crop_size, xx:xx + crop_size]
         raw_matrix = np.maximum(raw_matrix - 512, 0) / (16383 - 512)
 #         print(raw_matrix.shape)
         raw_matrix = np.expand_dims(np.float32(raw_matrix), axis=2)
